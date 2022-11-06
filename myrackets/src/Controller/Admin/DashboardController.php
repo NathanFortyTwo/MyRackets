d9fa4dab67c97ea0d5b610ff75caba4a06baae0c
= <=>
2022-11-06 23:51:18 +0100

    empty twig + tennisman crud

--- myrackets/src/Controller/Admin/DashboardController.php
+++ myrackets/src/Controller/Admin/DashboardController.php
@@ -19,7 +19,8 @@ class DashboardController extends AbstractDashboardController
         // redirect to some CRUD controller
         $routeBuilder = $this->get(AdminUrlGenerator::class);
 
-        return $this->redirect($routeBuilder->setController(InventoryCrudController::class)->generateUrl());
+        //return $this->redirect($routeBuilder->setController(InventoryCrudController::class)->generateUrl());
+        return $this->redirect($routeBuilder->setController(TennisManCrudController::class)->generateUrl());
 
         // you can also redirect to different pages depending on the current user
         if ('jane' === $this->getUser()->getUsername()) {
